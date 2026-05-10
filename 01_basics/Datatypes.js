ec597acad9af20e8e72f76156621567ae9bcc83b
Chinuu982 <chanchalapatel070@gmail.com>
2026-05-10 11:18:32 +0000

    Datatypes and variables

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,25 @@
+"use strict"  // treat jada code as a newer version
+
+// alert(3+3);    we are using nodejs not browser isliye error aari h
+
+
+// note : js ki do documentation hoti h 
+// 1) TC39     2) MDN
+
+
+let age = 18;
+let isloggedIn = false;
+let state = null;
+
+console.log(typeof isloggedIn);     // data ka type
+console.log(typeof null);         // object
+console.log(typeof undefined);       // undefined
+
+// Datatypes ->
+// number -> 2 to power 50
+// bigint
+// string = " "
+// boolean = true / false
+// null => standalone value 
+// symbol => unique
+//object
---
+++ 01_basics/Variables.js
@@ -0,0 +1,15 @@
+const accountId = 1234;   // constant value not changed
+let accountEmail = "chinu@345t.com";    
+var accountPassword = "chin234";
+accountCity = "jaipur"   // bina let , var k execute bi ho skta h 
+
+accountEmail = "fjvgjgf@.com";
+accountPassword = "ff56878";
+
+let accountState;
+// agar declare krke chor do to js = o/p dega undefined
+
+console.log(accountId , accountEmail , accountPassword, accountCity);
+ 
+// note : var kbhi use mat kro , bcz of issue in block scope and function scope
+// let and const use kre 
